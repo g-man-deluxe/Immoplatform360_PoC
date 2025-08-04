ab51ff609d92f72d6b06c3718819eb7c8d914b13
g-man-deluxe <achim.guttmann@gmail.com>
2025-08-04 21:22:34 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-from views import quick_check, dashboard, arbeitsvorrat
+import quick_check
+import dashboard
+import arbeitsvorrat
 
 st.set_page_config(page_title="Immo360", layout="wide")
-st.sidebar.image("assets/logo.png", use_column_width=True)
+st.sidebar.image("logo.png", use_column_width=True)
 st.sidebar.title("Immo360 Navigation")
 
 page = st.sidebar.radio("Seite auswählen", ["Dashboard", "Quick Check", "Arbeitsvorrat"])
